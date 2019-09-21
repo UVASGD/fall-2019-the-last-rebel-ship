dbfe341094e233537e760b261653449afa9d7a5a
MAKtheUnknown <mak4em@virginia.edu>
2019-09-20 20:05:25 -0400

    Created Space Mines
    It doesn't kill yey, but whatever.

---
+++ The Last Rebel/Assets/Script/SpaceMine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceMine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        Transform location = player.transform;
+
+        this.GetComponent<Rigidbody2D>().AddForce(new Vector2(-(this.transform.position.x - location.position.x), -(this.transform.position.y - location.position.y)));
+
+
+    }
+}

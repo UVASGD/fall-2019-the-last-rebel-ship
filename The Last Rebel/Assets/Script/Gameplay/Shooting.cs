540ae5927ca57a8bb7136f44d33b6ade2209cf17
TepSpaceCat <jhf8fjb@virginia.edu>
2019-09-27 15:29:48 -0400

    Creation of the BasicBullet

---
+++ The Last Rebel/Assets/Script/Gameplay/Shooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform FirePoint;
+    public GameObject bulletPrefab;
+    public float bulletForce = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(FirePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}

77214d2991239997380cc3f01d8d4fb4555f422f
jherd23 <jherd23@gmail.com>
2019-10-14 17:53:12 -0400

    Outline done

---
+++ The Last Rebel/Assets/Script/Chunks/Chunk.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+
+    #region Static Fields
+
+    #endregion
+
+    #region Instance Fields
+
+    protected Vector2Int position;
+    private Random.State prng;
+
+    #endregion
+
+    #region Unity Methods
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region Class Methods
+
+    public void SetPosition(Vector2Int _position) {
+        position = _position;
+
+        Random.State exogenous_state = Random.state;
+
+        Random.InitState((position.y % ChunkManager.MODULUS) * ChunkManager.MODULUS + (position.x % ChunkManager.MODULUS));
+
+        prng = Random.state;
+
+        Random.state = exogenous_state;
+    }
+
+    protected float GetSeededRandomNumber() {
+        Random.State exogenous_state = Random.state;
+
+        Random.state = prng;
+
+        float r = Random.value;
+
+        prng = Random.state;
+
+        Random.state = exogenous_state;
+
+        return r;
+    }
+
+    public virtual void Allocate() {
+
+    }
+
+    public virtual void DeAllocate() {
+
+    }
+
+    #endregion
+}
---
+++ The Last Rebel/Assets/Script/Chunks/ChunkManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkManager : MonoBehaviour
+{
+    #region Static Fields
+
+    public static readonly int MODULUS = 10;
+
+    public static readonly float CHUNK_SIZE = 10;
+
+    public static readonly int MAX_LOADED_CHUNKS = 18;
+
+    #endregion
+
+    #region Instance Fields
+
+    public GameObject player;
+
+    private Vector2Int playerPosition;
+
+    private Dictionary<Vector2Int, Chunk> chunks;
+
+    #endregion
+
+    #region Unity Methods
+
+    // Start is called before the first frame update
+    void Start() {
+        if(!player) {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector2Int currPosition = new Vector2Int(
+            (int)(player.transform.position.x / CHUNK_SIZE),
+            (int)(player.transform.position.y / CHUNK_SIZE)
+        );
+
+        if(currPosition.x != playerPosition.x || currPosition.y != playerPosition.y) {
+            playerPosition = currPosition;
+            RecalculateChunks();
+        }
+    }
+
+    #endregion
+
+    #region Class Methods
+
+    public void RecalculateChunks() {
+        foreach(Vector2Int pos in Adjacents(playerPosition)) {
+            AllocateChunk(pos);
+        }
+    }
+
+    private void FreeChunk() {
+        if (chunks.Count == 0) {
+            Debug.Log("Something's fucky in the state of denmark");
+        }
+
+        Vector2Int max_position = playerPosition;
+
+        foreach(Vector2Int pos in chunks.Keys) {
+            if((playerPosition - pos).sqrMagnitude > (max_position - playerPosition).sqrMagnitude) {
+                max_position = pos;
+            }
+        }
+
+        Chunk toRemove = chunks[max_position];
+
+        chunks.Remove(max_position);
+
+        toRemove.DeAllocate();
+    }
+
+    private void AllocateChunk(Vector2Int position) {
+        if(chunks.Count > MAX_LOADED_CHUNKS) {
+            FreeChunk();
+        }
+
+        Chunk toAdd = ChunkFactory.ProduceChunk();
+
+        chunks[position] = toAdd;
+
+        toAdd.SetPosition(position);
+        toAdd.Allocate();
+    }
+
+    private Vector2Int[] Adjacents(Vector2Int position) {
+        return new Vector2Int[] {
+            new Vector2Int(position.x - 1, position.y - 1), new Vector2Int(position.x, position.y - 1)  , new Vector2Int(position.x + 1, position.y - 1),
+            new Vector2Int(position.x - 1, position.y)    ,                                               new Vector2Int(position.x + 1, position.y),
+            new Vector2Int(position.x - 1, position.y + 1), new Vector2Int(position.x, position.y + 1)  , new Vector2Int(position.x + 1, position.y + 1),
+        };
+    }
+
+    #endregion
+}

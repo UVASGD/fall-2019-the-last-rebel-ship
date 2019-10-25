a059582290ba698fcb3febed7d761be4e5859bd4
TepSpaceCat <jhf8fjb@virginia.edu>
2019-10-25 17:56:39 -0400

    Particle System Things

--- The Last Rebel/Assets/Script/Gameplay/Death.cs
+++ The Last Rebel/Assets/Script/Gameplay/Death.cs
@@ -5,9 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Death : MonoBehaviour
 {
+    //public ParticleSystem deathParticles;
+
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D other)
     {
+
+        StartCoroutine(DeathParticles());
         SceneManager.LoadScene("Death Scene");
+    }
+
+    IEnumerator DeathParticles()
+    {
+        //deathParticles.Play;
+        yield return new WaitForSeconds(5);
     }
 }

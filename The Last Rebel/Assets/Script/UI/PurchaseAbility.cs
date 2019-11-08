8dbbb4537d854b3d3152527e74e721a4b7f948a3
arh4uwe <55301499+arh4uwe@users.noreply.github.com>
2019-11-08 15:49:00 -0500

    Added menu
    There is a powerup menu that you can summon and banish by pressing Tab. There is also a button that changes its text when you click it.

---
+++ The Last Rebel/Assets/Script/UI/PurchaseAbility.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PurchaseAbility : MonoBehaviour
+{
+    
+    public void SetText(string text)
+    {
+        Text t = transform.Find("Text").GetComponent<Text>();
+        t.text = text;
+    }
+
+}

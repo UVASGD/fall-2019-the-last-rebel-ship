b15d81b49d6e659d5d8b1786973df3ac05a99f7a
Kevin Li <kl7ck@virginia.edu>
2019-11-08 16:30:12 -0500

    made basic spawner

--- The Last Rebel/Assets/Script/Gameplay/SpawnShips.cs
+++ The Last Rebel/Assets/Script/Gameplay/SpawnShips.cs
@@ -29,23 +29,23 @@ public class SpawnShips : MonoBehaviour
     void Spawn()
     {
         GameObject shipn = Instantiate(shipPrefab, spawnPointN.position, spawnPointN.rotation);
-        /*Rigidbody2D rbn = shipn.GetComponent<Rigidbody2D>();
+        Rigidbody2D rbn = shipn.GetComponent<Rigidbody2D>();
         rbn.velocity = GetComponent<Rigidbody2D>().velocity;
-        rbn.AddForce((new Vector2(0, shipLanuchForce)));*/
+        rbn.AddForce((new Vector2(0, shipLanuchForce)));
 
         GameObject shipe = Instantiate(shipPrefab, spawnPointE.position, spawnPointE.rotation);
-        /*Rigidbody2D rbe = shipe.GetComponent<Rigidbody2D>();
+        Rigidbody2D rbe = shipe.GetComponent<Rigidbody2D>();
         rbe.velocity = GetComponent<Rigidbody2D>().velocity;
-        rbe.AddForce((new Vector2(shipLanuchForce, 0)));*/
+        rbe.AddForce((new Vector2(shipLanuchForce, 0)));
 
         GameObject ships = Instantiate(shipPrefab, spawnPointS.position, spawnPointS.rotation);
-        /*Rigidbody2D rbs = ships.GetComponent<Rigidbody2D>();
+        Rigidbody2D rbs = ships.GetComponent<Rigidbody2D>();
         rbs.velocity = GetComponent<Rigidbody2D>().velocity;
-        rbs.AddForce((new Vector2(0, -1 * shipLanuchForce)));*/
+        rbs.AddForce((new Vector2(0, -1 * shipLanuchForce)));
 
         GameObject shipw = Instantiate(shipPrefab, spawnPointW.position, spawnPointW.rotation);
-        /*Rigidbody2D rbw = shipw.GetComponent<Rigidbody2D>();
+        Rigidbody2D rbw = shipw.GetComponent<Rigidbody2D>();
         rbw.velocity = GetComponent<Rigidbody2D>().velocity;
-        rbw.AddForce((new Vector2(-1 * shipLanuchForce, 0)));*/
+        rbw.AddForce((new Vector2(-1 * shipLanuchForce, 0)));
     }
 }

3fb99e6f1f8c4d02f5dd63fbf3359ff26e48630a
MAKtheUnknown <mak@rehost.com>
2019-11-15 18:24:02 -0800

    Spawner is now an absolute unit.

--- The Last Rebel/Assets/Script/Gameplay/PowerupManager.cs
+++ The Last Rebel/Assets/Script/Gameplay/PowerupManager.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PowerupManager : MonoBehaviour
 {
 
-
+    public float[] shieldCapacity = { 100, 200, 500 };
+    public float[] acceleration = { 5, 10, 30, 50, 100 };
+    public float[] bulletSpeed = { 10, 20, 30 };
+    public float[] gravityGun = { 50, 500, 1000 };
+    public float[] tractorBeam = { 1 };
 
     // Start is called before the first frame update
     void Start()

fe1ae530feef78c4c693ae18b6798ca6a3f0dfad
MAKtheUnknown <mak@rehost.com>
2019-11-15 19:19:19 -0800

    Worked on Powerups

---
+++ The Last Rebel/Assets/Script/Gameplay/Powerup.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+
+    public float[] powerarray;
+    public int[] costarray;
+    public int currentPower = 0;
+    public string name = "";
+
+    public GameObject label;
+    public GameObject costLabel;
+    public GameObject button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        (label.GetComponent<Text>()).text = name;
+        (costLabel.GetComponent<Text>()).text = costarray[0] + " gb";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Upgrade()
+    {
+        if (currentPower + 1 < powerarray.Length)
+        {
+            currentPower++;
+        }
+    }
+
+    public float GetValue()
+    {
+        return powerarray[currentPower];
+    }
+
+    public float GetCost()
+    {
+        return costarray[currentPower];
+    }
+}
--- The Last Rebel/Assets/Script/Gameplay/PowerupManager.cs
+++ The Last Rebel/Assets/Script/Gameplay/PowerupManager.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 public class PowerupManager : MonoBehaviour
 {
 
-    public float[] shieldCapacity = { 100, 200, 500 };
-    public float[] acceleration = { 5, 10, 30, 50, 100 };
-    public float[] bulletSpeed = { 10, 20, 30 };
-    public float[] gravityGun = { 50, 500, 1000 };
-    public float[] tractorBeam = { 1 };
+    public Powerup[] powerups;
 
     // Start is called before the first frame update
     void Start()

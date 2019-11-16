481102caa9b3025e92b78b669807da9375d16fa2
MAKtheUnknown <mak@rehost.com>
2019-11-15 16:17:32 -0800

    BEEG Spawner

---
+++ The Last Rebel/Assets/Script/SceneChange.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void GotoHomeScreenScene()
+    {
+        SceneManager.LoadScene("HomeScreen");
+    }
+}

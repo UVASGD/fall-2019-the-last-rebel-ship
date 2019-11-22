356fcfd8b6f44e1ef62f115b649d90ad2cb7be98
MAKtheUnknown <mak@rehost.com>
2019-11-22 13:53:16 -0800

    Turret

--- The Last Rebel/Assets/Script/Gameplay/ShotgunShooting.cs
+++ The Last Rebel/Assets/Script/Gameplay/ShotgunShooting.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
 
 public class ShotgunShooting : MonoBehaviour
 {
@@ -13,11 +16,12 @@ public class ShotgunShooting : MonoBehaviour
 	public float bulletForce = .1f;
 	public float timeBetweenShots = .333333f;  // Allow 3 shots per secon
 	private float timestamp;
+    public Text test;
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (Time.time >= timestamp && (Input.GetKey(KeyCode.S)))
+		if (Time.time >= timestamp && (Input.GetKey(KeyCode.B)))
 		{
 			Shoot();
 			timestamp = Time.time + timeBetweenShots;
@@ -32,9 +36,16 @@ public class ShotgunShooting : MonoBehaviour
 		GameObject bullet = Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
 		GameObject bullet2 = Instantiate(bulletPrefab, FirePoint2.position, FirePoint2.rotation);
 		GameObject bullet3 = Instantiate(bulletPrefab, FirePoint3.position, FirePoint3.rotation);
+        Rigidbody2D ship = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMove>().rb;
+        var velValue = ship.velocity.magnitude;
+        float force = velValue+bulletForce;
+
+        bullet.AddComponent<BounceBullet>();
+        bullet2.AddComponent<BounceBullet>();
+        bullet3.AddComponent<BounceBullet>();
 
 
-		Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
 		Rigidbody2D rb2 = bullet2.GetComponent<Rigidbody2D>();
 		Rigidbody2D rb3 = bullet3.GetComponent<Rigidbody2D>();
 
@@ -45,11 +56,14 @@ public class ShotgunShooting : MonoBehaviour
 		Collider2D c3 = bullet3.GetComponent<Collider2D>();
 		Physics2D.IgnoreCollision(c3, GetComponent<Collider2D>());
 
-		rb.AddForce(FirePoint.up * bulletForce, ForceMode2D.Impulse);
-		rb2.AddForce(right45 * bulletForce, ForceMode2D.Impulse);
-		rb3.AddForce(left45 * bulletForce, ForceMode2D.Impulse);
 
 
+        rb.AddForce(FirePoint.up * force, ForceMode2D.Impulse);
+		rb2.AddForce(right45 * force, ForceMode2D.Impulse);
+		rb3.AddForce(left45 * force, ForceMode2D.Impulse);
 
-	}
+
+        test.text = (FirePoint.up * force).ToString();
+
+    }
 }
---
+++ The Last Rebel/Assets/Script/Gameplay/TurretShoot.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretShoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform FirePoint;
+    public GameObject bulletPrefab;
+    public float bulletForce = .1f;
+    public float range = 5;
+    public float fireRate = 2;
+    private float timeBetweenShots;
+    private float timestamp;
+    private Transform target;
+    private Collider2D spawner;
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeBetweenShots = (1 / fireRate);
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        spawner = transform.parent.gameObject.GetComponent<Collider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 raycastDir = target.position - transform.position;
+
+        if (Time.time >= timestamp && (raycastDir.magnitude < range))
+        {
+            Shoot();
+            timestamp = Time.time + timeBetweenShots;
+        }
+    }
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
+        bullet.AddComponent<EnemyBullet>();
+        Collider2D c = bullet.GetComponent<Collider2D>();
+        //Physics2D.IgnoreCollision(c, GetComponent<Collider2D>());
+        Physics2D.IgnoreCollision(c, spawner);
+        //Physics2D.IgnoreCollision(c, lr);
+        //Physics2D.IgnoreCollision(c, ur);
+        //Physics2D.IgnoreCollision(c, ll);
+        //Physics2D.IgnoreCollision(c, ul);
+
+
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(FirePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
+
+
+
